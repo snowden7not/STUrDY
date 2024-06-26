7753da7e3bdc9b91cd84338ceec70ac5f60b49a5
Lakhan Kumar <109954175+snowden7not@users.noreply.github.com>
2024-06-26 14:41:02 +0530

    Update apis.js
    Updating backend URL in frontend

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,6 @@
 
-const BASE_URL = "http://localhost:4000/api/v1"
+//const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://sturdy-backend.onrender.com/api/v1"
                       
 // AUTH ENDPOINTS
 export const endpoints = {
