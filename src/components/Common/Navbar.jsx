e2298dd8951cacb2c2b244c1c0f93d31695d3606
Lakhan Kumar <109954175+snowden7not@users.noreply.github.com>
2024-06-27 12:39:29 +0530

    Update Navbar.jsx

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -67,7 +67,7 @@ function Navbar() {
   };
 
   return (
-  <div className="flex flex-col">
+  <div className="">
     <div
       className={`flex md:h-14 h-12 items-center justify-center border-b-[1px] border-b-richblack-700 ${
         location.pathname !== "/" ? "bg-richblack-800" : ""
